6bf770104478af43176ae681dbc0b5b0d0b4f3f3
Abhijnakotyan <aabhijnakotyan@gmail.com>
2024-09-27 13:41:45 +0530

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android") // Kotlin plugin
 }
 
 android {
@@ -17,42 +17,21 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
-repositories {
-    google()
-    mavenCentral()
-}
-
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
+    implementation(libs.androidx.core.ktx)           // Correct reference
+    implementation(libs.androidx.appcompat)          // Correct reference
+    implementation(libs.material)                    // Correct reference
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.android.holo.graph)
+    implementation(libs.mpandroidchart)
+    implementation(libs.holographlibrary)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.holographlibrary)
-    implementation(libs.android.holographlibrary)
-    implementation(libs.mpandroidchart)
-    implementation(libs.android.holo.graph)
 }
